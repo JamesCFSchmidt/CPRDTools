db27e3ef2cbc1ed346dcd60982f40f892d46bad5
JamesCFSchmidt <jcfs2@leicester.ac.uk>
2022-07-05 14:53:30 +0100

    created first_n

---
+++ R/first_n.R
@@ -0,0 +1,33 @@
+#' Display the first n rows (records)
+#'
+#' @param db_path The file path to the database location.
+#' @param table_name The name of the table to be interrogated.
+#' @param n Integer of the number of rows to be displayed (default = 10)
+#'
+#' @export
+#'
+first_n <- function(db_path,
+                    table_name,
+                    n){
+  if(missing(db_path)){
+    stop("Specify database file path location")}
+  if(missing(table_name)){
+    stop("Specify table name")}
+  if(!any(is.character(c(db_path,table_name)))){
+    stop("Specify db_path, table_name as.char")}
+  sqlite <- DBI::dbDriver("SQLite")
+  connex <- DBI::dbConnect(RSQLite::SQLite(),dbname=paste0(db_path,"/database.db"))
+  loaded_tables <- tolower(DBI::dbListTables(connex))
+  if(tolower(table_name) %in% loaded_tables==F){
+    stop("Review table spcified")}
+  if(missing(n)){n=10}
+  query <- paste0("SELECT * FROM ",table_name," LIMIT ",n,";")
+  query_data <- data.frame(DBI::dbGetQuery(connex,query))
+  DBI::dbDisconnect(connex)
+  out_list <- list("database_location" = db_path,
+                   "query" = query,
+                   "first_n_rows" = query_data
+  )
+  return(out_list)
+  rm(connex,sqlite,db_path,table_name,n,query,query_data,loaded_tables)
+}
