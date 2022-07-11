dd6791a618a01f84f1ee959af00e003aa8a6e149
JamesCFSchmidt <jcfs2@leicester.ac.uk>
2022-07-11 13:35:26 +0100

    object.size rather than file.size

--- R/load_global.R
+++ R/load_global.R
@@ -34,7 +34,7 @@ load_global <- function(db_path,
                append=T)
   message(cat(crayon::green(paste0("----------LOAD OF TABLE ",table_name," SUCCESSFUL----------\n"))))
   loaded_tables <- DBI::dbListTables(connex)
-  load <- data.frame("Tab"=table_name,"byte"=as.numeric(file.size(file_to_load)))
+  load <- data.frame("Tab"=table_name,"byte"=as.numeric(utils::object.size(file_to_load)))
   loaded_files <- data.frame(cbind(load$Tab,round(load$byte/1048576,4),round(load$byte/1073741824,4)))
   names(loaded_files) <- c("table","size_Mb","size_Gb")
   end_time <- Sys.time()
